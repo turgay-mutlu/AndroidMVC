21532ccec95109151d815c9b01af12d0b13f7f27
Turgay Mutlu <mutlu.turgay@gmail.com>
2018-11-10 20:00:07 +0300

    Models add

--- app/src/main/java/com/example/turgay/androidmvc/view/MainActivity.java
+++ app/src/main/java/com/example/turgay/androidmvc/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.turgay.androidmvc;
+package com.example.turgay.androidmvc.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.turgay.androidmvc.R;
 
 public class MainActivity extends AppCompatActivity {
 
